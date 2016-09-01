82ddd83f706d93a5038748456b88fb7701f6047f
bardack32 <bardack32@misena.edu.co>
2016-08-31 19:32:09 -0500

    Version 1.2
    Funcionalidad

--- RegistroEstudiantes/js/js.js
+++ RegistroEstudiantes/js/js.js
@@ -1,1 +1,7 @@
-﻿
+﻿function mostrarGuardado() {
+    var nombre = document.getElementById("txtNombre").value;
+    var apellido = document.getElementById("txtApellidos").value;
+    var ciudad = document.getElementById("txtCiudad").value;
+    var celular = document.getElementById("txtCelular").value;
+    alert("Estudiante " + nombre + " " + apellido + " usted vive en " + ciudad + " y su teléfono de contacto es " + celular + ". sus datos han sido registrados en nuestro sistema");
+}
